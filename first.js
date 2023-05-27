655f77d5bc533e462b6d76314a3cd4a0506a26de
Ruhul Amin <ruhulaminswapon93@gmail.com>
2023-05-27 18:16:35 +0600

    added

--- first.js
+++ first.js
@@ -1,1 +1,22 @@
+
+// Numeric
 var price = 25;
+
+// String
+var person = "Sodor Uddin" ;
+
+// Boolean
+var isSingle = true;
+
+//
+var isSingle = true;
+
+var onioPrice = 30;
+console.log(onioPrice);
+
+
+var money = 500;
+var player = 10;
+
+var totalCost = money / player;
+console.log(totalCost);
