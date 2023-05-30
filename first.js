cb13a373f5d16c457b202d01e22ea6dd8bddf80e
Ruhul Amin <ruhulaminswapon93@gmail.com>
2023-05-30 19:41:58 +0600

    added

--- first.js
+++ first.js
@@ -1,23 +1,30 @@
 
-// Numeric
-var price = 25;
+// // Numeric
+// var price = 25;
 
-// String
-var person = "Sodor Uddin" ;
+// // String
+// var person = "Sodor Uddin" ;
 
-// Boolean
-var isSingle = true;
+// // Boolean
+// var isSingle = true;
 
-//
-var isSingle = true;
+// //
+// var isSingle = true;
 
-var onioPrice = 30;
-console.log(onioPrice);
+// var onioPrice = 30;
+// console.log(onioPrice);
 
 
-var money = 500;
-var player = 10;
-player = 20;
+// var money = 500;
+// var player = 10;
+// player = 20;
 
-var totalCost = money / player;
-console.log(totalCost);
+// var totalCost = money / player;
+// console.log(totalCost);
+
+
+var firstName = Jennie;
+var lastName = Kim;
+var fullName = firstName + ' ' + lastName;
+var fullName = firstName + ' ' + lastName;
+console.log(fullName);
