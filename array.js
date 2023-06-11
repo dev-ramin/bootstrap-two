3b7716304c3926df5d6d6e8813aafb8d38702ff1
Ruhul Amin <ruhulaminswapon93@gmail.com>
2023-06-11 14:00:11 +0600

    added

--- array.js
+++ array.js
@@ -1,3 +1,12 @@
 var nayok = [1000, 2000, 5000];
 var vowels = ['a', 'e', 'i', 'o', 'u'];
-console.log(vowels);
+console.log(vowels[2]);
+
+
+var something = [54, 68, 89,88];
+console.log(something[2]);
+something[2] = 99;
+console.log(something[2]);
+
+var positionIndex = something.indexOf(88);
+console.log(positionIndex);
